fd9b9d82f16402a7647ef7cffeaef4aba85309a1
leticiaacs-dev <leticia.aurora@gmail.com>
2025-02-08 14:51:56 -0300

    add line on plot bars

--- analytics.py
+++ analytics.py
@@ -22,6 +22,8 @@ if col2.button("➡ Próxima Página"):
 
 ##################################### Assuntos #####################################
 
+# Relatório 1: Entregas
+
 if st.session_state.pagina == 1:
     st.subheader("Relatório 1: Entregas")
     st.write("Principais KPIs sobre a área de Entregas")
@@ -54,22 +56,36 @@ if st.session_state.pagina == 1:
         # Criar DataFrame com todos os meses e preencher com zero quando necessário
         df_mensal = df_filtrado.groupby('Mês', observed=True)['N de viagens'].sum().reindex(ordem_meses, fill_value=0).reset_index()
 
+        # Calcular a média de KMs para cada mês
+        media_kms_por_mes = df_filtrado.groupby('Mês')['Media Kms'].mean().reindex(ordem_meses, fill_value=0)
+
         # Gerar gráfico
         plt.figure(figsize=(8, 6))
-        bars = plt.bar(df_mensal['Mês'], df_mensal['N de viagens'], color='royalblue')
+        bars = plt.bar(df_mensal['Mês'], df_mensal['N de viagens'], color='royalblue', label='Quant. de entregas feitas')
         
-        # Adicionar rótulos nas barras
+        # Adicionar rótulos nas barras, centralizados
         for bar in bars:
             height = bar.get_height()
             if height > 0:
-                plt.text(bar.get_x() + bar.get_width()/2, height, int(height), 
-                         ha='center', va='bottom', fontsize=10)
+                plt.text(bar.get_x() + bar.get_width()/2, height/2, int(height), 
+                         ha='center', va='center', fontsize=10, color='white')
+
+        # Adicionar linha para a média de KMs com pontos conectados
+        plt.plot(df_mensal['Mês'], media_kms_por_mes, color='red', marker='o', linestyle='-', lw=2, label='Média de KMs rodados')
+
+        # Adicionar valores nos pontos da linha
+        for i, value in enumerate(media_kms_por_mes):
+            plt.text(df_mensal['Mês'][i], value, f'{value:.2f}', ha='center', va='bottom', color='red', fontsize=10)
 
         plt.title(f"Quantidade de entregas por mês em {ano}")
         plt.xlabel('Mês')
         plt.ylabel('Quantidade de entregas')
         plt.ylim(0, 60)  # Definir eixo Y fixo de 0 a 60
         plt.xticks(rotation=45)
+        
+        # Exibir legenda
+        plt.legend(loc='upper left')
+        
         st.pyplot(plt)
 
     # Carregar os dados
